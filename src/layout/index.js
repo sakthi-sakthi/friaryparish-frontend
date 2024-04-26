8150c90de8af55bec72e8d5c0e33c1e77388fe6c
Muniraj M <107682409+Muniraj132@users.noreply.github.com>
2024-04-26 12:02:18 +0530

    change the desings in subpage

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./../components/header";
 import Footer from "../components/footer";
-
+import { useLocation } from "react-router-dom";
 const MainLayout = () => {
+  let location = useLocation();
+  const url = location.pathname;
+  const [pageTitle, setPageTitle] = useState("");
+  useEffect(() => {
+  const path = {
+    "/history": "History",
+    "/our-vision": "Our Vision",
+  };
+  setPageTitle(path[url] ? path[url] : url);
+}, [url]);
   return (
     <>
       <Header />
-      <div style={{ minHeight: "66.5vh", padding: "10px" }}>
+      <div style={{ minHeight: "66.5vh" }}>
+      <div className="home-mother">
+  <div className="container" style={{ padding: 30 }}>
+    <h3 className="entry-title motherhouse">
+      <a href="/motherhouse">{pageTitle}</a>
+    </h3>
+    <div className="brudcrums">
+      <a href="/">Home &nbsp;»&nbsp; </a>
+      <span className="pagename">{pageTitle}</span>
+    </div>
+  </div>
+</div>
+
         <Outlet />
       </div>
       <Footer />
